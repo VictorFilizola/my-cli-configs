c571b8eb6399c41fe7c41b3579838f97c42b15dc
fili <51762928+VictorFilizola@users.noreply.github.com>
2026-07-26 21:43:50 -0300

    feat: centralize wezterm settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 -- General Window Options
 config.window_background_opacity = 0.75
-config.window_padding = { left = 8, right = 8, top = 6, bottom = 6 }
+config.window_padding = { left = 3, right = 0, top = 0, bottom = 0 }
 config.scrollback_lines = 10000
 
 -- Mux (tmux-like persistence)
@@ -15,7 +15,7 @@ config.default_gui_startup_args = { "connect", "unix" }
 
 -- Font Configuration
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 14
+config.font_size = 13
 
 -- Disable new tab button
 config.show_new_tab_button_in_tab_bar = false
@@ -28,7 +28,7 @@ config.show_new_tab_button_in_tab_bar = false
 config.enable_tab_bar = true
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 
 -- Color Palette (VSCode Dark)
 config.colors = {
@@ -174,5 +174,4 @@ wezterm.on("gui-attached", function(_domain)
 		end
 	end
 end)
-
 return config
